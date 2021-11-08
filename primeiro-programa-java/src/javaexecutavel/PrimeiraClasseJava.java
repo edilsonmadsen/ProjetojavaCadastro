d2a71ebcbdd3bed5aa80016e55f38c7eed394089
Edilson Madsen <edilson_madsen@hotmail.com>
2021-11-08 11:33:07 -0300

    Criação do Projeto

---
+++ primeiro-programa-java/src/javaexecutavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package javaexecutavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um método auto executavel em java*/
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa em Java");
+		
+	}
+
+}
